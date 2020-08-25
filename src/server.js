41ff384bf02fd75654f5a09b7aaa8a1662d3f27b
dave <daveg7lee@gmail.com>
2020-08-25 19:26:04 +0900

    Chat with SocketIO

--- src/server.js
+++ src/server.js
@@ -2,7 +2,6 @@ import { join } from "path";
 import express from "express";
 import socketIO from "socket.io";
 import logger from "morgan";
-
 const PORT = 4000;
 const app = express();
 app.set("view engine", "pug");
@@ -10,14 +9,20 @@ app.set("views", join(__dirname, "views"));
 app.use(logger("dev"));
 app.use(express.static(join(__dirname, "static")));
 app.get("/", (req, res) => res.render("home"));
-
 const handleListening = () =>
-  console.log(`Server running: http://localhost:${PORT}`);
-
+  console.log(`✅ Server running: http://localhost:${PORT}`);
 const server = app.listen(PORT, handleListening);
-
 const io = socketIO.listen(server);
 
 io.on("connection", (socket) => {
   socket.on("helloGuys", () => console.log("the client said hello"));
+  socket.on("newMessage", ({ message }) => {
+    socket.broadcast.emit("messageNotif", {
+      message,
+      nickname: socket.nickname || "Anon",
+    });
+  });
+  socket.on("setNickname", ({ nickname }) => {
+    socket.nickname = nickname;
+  });
 });
